674f2b344c98315aa3ca713a9862c5822d0e0122
이경민 <74659491+meenyweeny@users.noreply.github.com>
2023-04-02 16:27:34 +0900

    feat #1 - 1차 세미나 파일 추가

---
+++ Seminar/firstSeminar/src/main/java/sopt/org/firstSeminar/FirstSeminarApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.firstSeminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSeminarApplication.class, args);
+	}
+
+}
---
+++ Seminar/firstSeminar/src/test/java/sopt/org/firstSeminar/FirstSeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.firstSeminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
